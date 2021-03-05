e49246535f1ac4919d726cc8ca577e37c211ba02
elfinwood-data-sci <71292876+elfinwood-data-sci@users.noreply.github.com>
2021-03-04 20:58:06 -0900

    added sql for selecting obervations with location withheld

---
+++ select_information_withheld.sql
@@ -0,0 +1,6 @@
+SELECT recordedby, nameoriginal, eventdate::timestamp with time zone
+	FROM public.gbif_csp_20210211_clipped_foa_taxonomy
+	WHERE informationwithheld IS NOT NULL
+AND informationwithheld NOT IN ('NA')
+AND recordedby NOT IN ('Aaron Wells')
+ORDER BY recordedby, nameoriginal
