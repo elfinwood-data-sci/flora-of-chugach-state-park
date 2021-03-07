2b93e6ad5548c443405e3a180b30c7e3a92dbb79
elfinwood-data-sci <71292876+elfinwood-data-sci@users.noreply.github.com>
2021-03-07 09:08:07 -0900

    minor edits

--- select_information_withheld.sql
+++ select_information_withheld.sql
@@ -1,6 +1,7 @@
-SELECT objectid, recordedby, nameoriginal, eventdate::timestamp with time zone, decimallatitude, decimallongitude
+SELECT objectid, recordedby, nameoriginal, eventdate::timestamp with time zone, decimallatitude, decimallongitude, observation_office_note
 	FROM public.gbif_csp_20210211_clipped_foa_taxonomy
 	WHERE informationwithheld IS NOT NULL
 AND informationwithheld NOT IN ('NA')
 AND recordedby NOT IN ('Aaron Wells')
+AND observation_office_note IS NULL 
 ORDER BY recordedby, nameoriginal
