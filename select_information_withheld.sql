7153455afd8242e503d4ba58b215e1bd2907114b
elfinwood-data-sci <71292876+elfinwood-data-sci@users.noreply.github.com>
2021-03-13 09:22:03 -0900

    minor edits

--- select_information_withheld.sql
+++ select_information_withheld.sql
@@ -1,8 +1,8 @@
-SELECT objectid, recordedby, nameoriginal, eventdate::timestamp with time zone, decimallatitude, decimallongitude, 
-observation_office_note, high_quality_location_data, location_in_chugach_state_park
+SELECT recordedby, objectid, nameoriginal, eventdate::timestamp with time zone, decimallatitude, decimallongitude, 
+observation_office_note, high_quality_location_data, location_in_chugach_state_park, informationwithheld
 	FROM public.gbif_csp_20210211_clipped_foa_taxonomy
 	WHERE informationwithheld IS NOT NULL
 AND informationwithheld NOT IN ('NA')
-AND recordedby IN ('Aaron Wells')
-AND observation_office_note IS NULL 
+AND recordedby NOT IN ('Aaron Wells')
+AND observation_office_note IS NULL
 ORDER BY recordedby, nameoriginal
