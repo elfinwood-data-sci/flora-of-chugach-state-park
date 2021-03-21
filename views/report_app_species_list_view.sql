f7e819f0118695a054fdaa4cdcecbc7f5dcd7181
awells <awells@abrinc.com>
2021-03-20 21:48:08 -0800

    added CTE and case statements to assign authors and families to fungi

--- views/report_app_species_list_view.sql
+++ views/report_app_species_list_view.sql
@@ -1,6 +1,20 @@
-SELECT familyy, nameaccepted, authoraccepted, habit, list, basisofrecord, high_quality_location_data, level
+WITH d AS (SELECT familyy, familyx, acceptedscientificname, nameaccepted, authoraccepted, authors, habit, list, basisofrecord, high_quality_location_data, level
 FROM public.gbif_csp_20210211_clipped_foa_taxonomy
+		   LEFT JOIN (SELECT taxon_name, name_status, authors FROM import.mycobank_taxon_list WHERE name_status = 'Legitimate') AS subq ON nameaccepted = taxon_name
 	WHERE location_in_chugach_state_park IS TRUE 
-		AND level NOT IN ('genus','NA')
-	GROUP BY familyy, nameaccepted, authoraccepted, habit, list, basisofrecord, high_quality_location_data, level
-	ORDER BY familyy, nameaccepted, basisofrecord, high_quality_location_data
+		AND level NOT IN ('genus','NA') 
+	GROUP BY familyy, familyx, acceptedscientificname,nameaccepted, authoraccepted,authors, habit, list, basisofrecord, high_quality_location_data, level
+	ORDER BY familyy, nameaccepted, basisofrecord, high_quality_location_data)
+	
+SELECT  
+	CASE
+		WHEN familyy = 'NA' THEN familyx
+		ELSE familyy
+	END::text AS family,
+	nameaccepted, 
+	CASE
+		WHEN authoraccepted = 'NA' THEN authors
+		ELSE authoraccepted
+	END::text AS authoraccepted,
+	habit, list, basisofrecord, high_quality_location_data, level
+FROM d
