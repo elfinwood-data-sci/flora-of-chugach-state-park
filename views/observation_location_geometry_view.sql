ee05cf499fda9ac7cd8b28060ba13d6dc0f3542f
awells <awells@abrinc.com>
2021-03-23 20:14:06 -0800

    added basisofrecord

--- views/observation_location_geometry_view.sql
+++ views/observation_location_geometry_view.sql
@@ -9,31 +9,35 @@ WITH gbif AS (SELECT gbifid, decimallatitude, decimallongitude,
 		WHEN nameaccepted = 'NA' THEN nameadjudicated
 		ELSE nameaccepted
 	END::text AS nameaccepted_with_fungi, level,
-	habit, native, nonnative, list,  location_in_chugach_state_park, high_quality_location_data
+	habit, native, nonnative, list,  location_in_chugach_state_park, high_quality_location_data,basisofrecord
 FROM public.gbif_csp_20210211_clipped_foa_taxonomy),
 
 vouchers2020 AS (
 SELECT project, family, v.scientific_name, collector, collection_number, month_collected, day_collected, year_collected, state, county, 
 locality, minimum_elevation, elevation_unit, lat_degrees, n_or_s, lon_degrees, w_or_e, geodetic_datum, coordinate_source, site_description, 
-specimen_notes, specimen_verifier, lat_deg_min_sec, long_deg_min_sec, high_quality_location_data,habit, level, native, nonnative, list
+specimen_notes, specimen_verifier, lat_deg_min_sec, long_deg_min_sec, high_quality_location_data,habit, level, native, nonnative, v.list,
+	'PRESERVED_SPECIMEN'::text AS basisofrecord
 	FROM public.chugach_state_park_vouchers_uaah_template_v02 v
 	LEFT JOIN voucher_metadata vm USING (scientific_name)
 	GROUP BY (project, family, v.scientific_name, collector, collection_number, month_collected, day_collected, year_collected, state, county, 
 locality, minimum_elevation, elevation_unit, lat_degrees, n_or_s, lon_degrees, w_or_e, geodetic_datum, coordinate_source, site_description, 
-specimen_notes, specimen_verifier, lat_deg_min_sec, long_deg_min_sec, high_quality_location_data,habit, level, native, nonnative, list)
+specimen_notes, specimen_verifier, lat_deg_min_sec, long_deg_min_sec, high_quality_location_data,habit, level, native, nonnative, v.list,
+			 'PRESERVED_SPECIMEN'::text)
 ),
 
 
 uni AS (SELECT 'gbif'::text AS obs_data_source, gbifid::text, decimallatitude, decimallongitude, nameaccepted_with_fungi, level,
-	habit, native, nonnative, list,  location_in_chugach_state_park, high_quality_location_data FROM gbif
+	habit, native, nonnative, list,  location_in_chugach_state_park, high_quality_location_data,basisofrecord 
+		FROM gbif
 	UNION
 	SELECT 'vouchers'::text AS obs_data_source, collection_number, lat_degrees, lon_degrees, scientific_name, 
-		level, habit, native::text, nonnative::text, list,  'TRUE'::boolean AS location_in_chugach_state_park, high_quality_location_data
+		level, habit, native::text, nonnative::text, list,  'TRUE'::boolean AS location_in_chugach_state_park, high_quality_location_data,
+		basisofrecord
 		FROM vouchers2020
 	   )
 
 SELECT ROW_NUMBER() OVER (ORDER BY gbifid) AS ogc_fid, obs_data_source, gbifid AS collection_number, decimallatitude, decimallongitude, nameaccepted_with_fungi AS scientific_name, level,
-	habit, native, nonnative, list,  location_in_chugach_state_park, high_quality_location_data,
+	habit, native, nonnative, list,  location_in_chugach_state_park, high_quality_location_data,basisofrecord,
 	ST_SetSRID(ST_MakePoint(decimallongitude, decimallatitude),4326) AS geom
 	FROM uni
 	--WHERE level NOT IN ('species','subspecies','variety','microspecies','hybrid')
