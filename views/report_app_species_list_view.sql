de29d118fe29a337d1f44aefc2471e444114fa4d
awells <awells@abrinc.com>
2021-03-24 20:04:58 -0800

    major refactor to fix the issues with the count of observations by taxon

--- views/report_app_species_list_view.sql
+++ views/report_app_species_list_view.sql
@@ -1,12 +1,12 @@
-WITH d AS (SELECT familyy, familyx, acceptedscientificname, nameaccepted, authoraccepted, authors, habit, list, basisofrecord, high_quality_location_data, level
+WITH d AS (SELECT gbifid::text, familyy, familyx, acceptedscientificname, nameaccepted, authoraccepted, authors, habit, list, basisofrecord, high_quality_location_data, level
 FROM public.gbif_csp_20210211_clipped_foa_taxonomy
 		   LEFT JOIN (SELECT taxon_name, name_status, authors FROM import.mycobank_taxon_list WHERE name_status = 'Legitimate') AS subq ON nameaccepted = taxon_name
 	WHERE location_in_chugach_state_park IS TRUE 
 		AND level NOT IN ('genus','NA') 
-	GROUP BY familyy, familyx, acceptedscientificname,nameaccepted, authoraccepted,authors, habit, list, basisofrecord, high_quality_location_data, level
+	--GROUP BY familyy, familyx, acceptedscientificname,nameaccepted, authoraccepted,authors, habit, list, basisofrecord, high_quality_location_data, level
 	ORDER BY familyy, nameaccepted, basisofrecord, high_quality_location_data),
 
-vouch AS (SELECT family AS famx, family AS famy, scientific_name, name_accepted, author, author, habit, list, 
+vouch AS (SELECT collection_number::text, family AS famx, family AS famy, scientific_name, name_accepted, author, author, habit, list, 
 		  'PRESERVED_SPECIMEN'::text AS basisofrecord, high_quality_location_data, level
 	FROM public.chugach_state_park_vouchers_uaah_template_v02
 	JOIN flora_of_alaska_view ON scientific_name = name_accepted),
@@ -15,7 +15,7 @@ uni AS (SELECT * FROM d
 	   UNION ALL
 	   SELECT * FROM vouch),
 	
-funguy AS (SELECT  
+funguy AS (SELECT gbifid,
 	CASE
 		WHEN familyy = 'NA' THEN familyx
 		ELSE familyy
@@ -28,40 +28,69 @@ funguy AS (SELECT
 	habit, list, basisofrecord, high_quality_location_data, level, basisofrecord || ': ' || high_quality_location_data AS basis_loc
 FROM uni),
 
-inatty_high AS (SELECT family, nameaccepted, authoraccepted, habit, list, level, count(nameaccepted) AS inat_h
-FROM funguy
+notsofunguy AS (
+	SELECT gbifid,family,nameaccepted,authoraccepted,habit,list,basisofrecord,high_quality_location_data,level,basis_loc
+	FROM funguy
+	GROUP BY gbifid,family,nameaccepted,authoraccepted,habit,list,basisofrecord,high_quality_location_data,level,basis_loc
+),
+
+inatty_high AS (SELECT family, nameaccepted, authoraccepted, habit, list, level, 'inat_h'::text AS attr, count(nameaccepted) AS cnt
+FROM notsofunguy
 WHERE basis_loc = 'HUMAN_OBSERVATION: true'
 GROUP BY family, nameaccepted, authoraccepted, habit, list, level),
 
-inatty_low AS (SELECT family, nameaccepted, authoraccepted, habit, list, level, count(nameaccepted) AS inat_l
-FROM funguy
+inatty_low AS (SELECT family, nameaccepted, authoraccepted, habit, list, level, 'inat_l'::text AS attr, count(nameaccepted) AS inat_l
+FROM notsofunguy
 WHERE basis_loc = 'HUMAN_OBSERVATION: false'
 GROUP BY family, nameaccepted, authoraccepted, habit, list, level),
 
-phys_high AS (SELECT family, nameaccepted, authoraccepted, habit, list, level, count(nameaccepted) AS phys_h
-FROM funguy
-WHERE basis_loc = 'PRESERVED_SPECIMEN: true'
+phys_high AS (SELECT family, nameaccepted, authoraccepted, habit, list, level, 'phys_h'::text AS attr, count(nameaccepted) AS phys_h
+FROM notsofunguy
+WHERE basis_loc IN ('PRESERVED_SPECIMEN: true','LIVING_SPECIMEN: true', 'MATERIAL_SAMPLE: true')
 GROUP BY family, nameaccepted, authoraccepted, habit, list, level),
 
-phys_low AS (SELECT family, nameaccepted, authoraccepted, habit, list, level, count(nameaccepted) AS phys_l
-FROM funguy
-WHERE basis_loc = 'PRESERVED_SPECIMEN: false'
+phys_low AS (SELECT family, nameaccepted, authoraccepted, habit, list, level, 'phys_l'::text AS attr, count(nameaccepted) AS phys_l
+FROM notsofunguy
+WHERE basis_loc IN ('PRESERVED_SPECIMEN: false','LIVING_SPECIMEN: false', 'MATERIAL_SAMPLE: false')
 GROUP BY family, nameaccepted, authoraccepted, habit, list, level),
 
-xyz AS (SELECT subq.family, subq.nameaccepted, subq.authoraccepted, subq.habit, subq.list, subq.level,
-	COALESCE(inat_h,0) AS inat_h,
-	COALESCE(inat_l,0) AS inat_l,
-	COALESCE(phys_h,0) AS phys_h,
-	COALESCE(phys_l,0) AS phys_l
-FROM (SELECT family, nameaccepted, authoraccepted, habit, list, level FROM funguy 
-	 GROUP BY family, nameaccepted, authoraccepted, habit, list, level
-	 ORDER BY authoraccepted) AS subq
-LEFT JOIN inatty_high USING (nameaccepted)
-LEFT JOIN inatty_low USING (nameaccepted)
-LEFT JOIN phys_high USING (nameaccepted)
-LEFT JOIN phys_low USING (nameaccepted)
-ORDER BY nameaccepted)
+unk_high AS (SELECT family, nameaccepted, authoraccepted, habit, list, level, 'unk_h'::text AS attr, count(nameaccepted) AS unk_h
+FROM notsofunguy
+WHERE basis_loc IN ('UNKNOWN: true')
+GROUP BY family, nameaccepted, authoraccepted, habit, list, level),
 
-SELECT * FROM xyz
+unk_low AS (SELECT family, nameaccepted, authoraccepted, habit, list, level, 'unk_l'::text AS attr, count(nameaccepted) AS unk_l
+FROM notsofunguy
+WHERE basis_loc IN ('UNKNOWN: false')
+GROUP BY family, nameaccepted, authoraccepted, habit, list, level),
 
-	 
+uni_two AS (
+SELECT * FROM inatty_high
+	UNION
+	SELECT * FROM inatty_low
+	UNION
+	SELECT * FROM phys_high
+	UNION
+	SELECT * FROM phys_low
+	UNION
+	SELECT * FROM unk_high
+	UNION
+	SELECT * FROM unk_low
+),
+
+xyz AS (SELECT family, nameaccepted, authoraccepted, habit, list, level,
+round(max((case when attr = 'inat_h' then cnt else 0 end)),0) as inat_h,
+round(max((case when attr = 'inat_l' then cnt else 0 end)),0) as inat_l,
+round(max((case when attr = 'phys_h' then cnt else 0 end)),0) as phys_h,
+round(max((case when attr = 'phys_l' then cnt else 0 end)),0) as phys_l,
+round(max((case when attr = 'unk_h' then cnt else 0 end)),0) as unk_h,
+round(max((case when attr = 'unk_l' then cnt else 0 end)),0) as unk_l
+FROM uni_two
+GROUP BY family, nameaccepted, authoraccepted, habit, list, level
+ORDER BY nameaccepted),
+
+tot AS (SELECT family, nameaccepted, authoraccepted, habit, list, level, inat_h, inat_l, phys_h, phys_l, unk_h, unk_l,
+inat_h + inat_l + phys_h + phys_l + unk_h + unk_l AS total_observations
+FROM xyz)
+
+SELECT * FROM tot
