b7d0f561c2dbf3e0ab673101e411530539d18134
awells <awells@abrinc.com>
2022-03-30 21:10:31 -0700

    minor edits to review new species obs in 2021

--- select_obs_2021_new.sql
+++ select_obs_2021_new.sql
@@ -1,10 +1,11 @@
-WITH d AS (SELECT ttt.habit, ttt.nameaccepted, tto.list
-FROM (SELECT habit, nameaccepted FROM public.gbif_csp_20220320_clipped_foa_taxonomy GROUP BY habit, nameaccepted) AS ttt
+WITH d AS (SELECT ttt.habit, ttt.nameaccepted, ttt.level, tto.list
+FROM (SELECT habit, nameaccepted, level FROM public.gbif_csp_20220320_clipped_foa_taxonomy GROUP BY habit, nameaccepted,level) AS ttt
 LEFT JOIN (SELECT scientific_name AS nameaccepted, list 
 	  FROM public.observation_location_geometry_view
 	  GROUP BY scientific_name, list) AS tto 
 	  USING (nameaccepted)
-WHERE list IS NULL 
+WHERE list IS NULL AND level IN ('species','subspecies','variety','microspecies','hybrid')
 ORDER BY ttt.nameaccepted)
 
 SELECT * FROM d
+WHERE habit NOT IN ('Fungi')
